72aa2b33b66f5467b3542b848872eab653579cec
AdamIshay <32616404+AdamIshay@users.noreply.github.com>
2018-07-10 19:39:03 -0400

    Update sample_DQN_target.py

--- sample_DQN_target.py
+++ sample_DQN_target.py
@@ -1,16 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on %(date)s
 
-@author: %(user)s
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on %(date)s
-
-@author: %(user)s
-"""
 
 import numpy as np
 import torch
@@ -41,35 +29,7 @@ class Agent(nn.Module):
         self.fc3=nn.Linear(128,7)
         self.bn_1=nn.BatchNorm1d(128)
         self.bn_2=nn.BatchNorm1d(128)
-        
-        
-        
-        #pdb.set_trace()
-        '''
-        self.layer1=nn.Sequential(nn.Linear(42,128),
-            nn.ReLU(),
-            nn.BatchNorm1d(128),
-            nn.Linear(128,128),
-            nn.ReLU(),
-            nn.BatchNorm1d(128),
-            nn.Linear(128,7),
-            nn.Softmax())
-        '''
-        '''
-        self.layer1=nn.Sequential(nn.Conv2d(1, 16, kernel_size=(4,3), stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(16),
-            nn.Conv2d(16, 16, kernel_size=2, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(16))
-            #nn.Linear(16*8,7),
-            #nn.Softmax())
-        
-        self.layer2=nn.Sequential(nn.Linear(16*8,128),nn.ReLU(),nn.BatchNorm1d(128),nn.Linear(128,7),nn.Softmax())
-        
-        '''
 
-        
         self.conv1 = nn.Sequential(
             nn.Conv2d(1, 16, kernel_size=(4,3), stride=1, padding=0),
             nn.BatchNorm2d(16),
@@ -81,8 +41,7 @@ class Agent(nn.Module):
             nn.ReLU())
         
         self.conv_fc=nn.Linear(2,7)
-        
-        
+
         self.opt=optim.RMSprop(self.parameters(),lr=learning_rate)
         
         self.run_type=0
@@ -94,34 +53,13 @@ class Agent(nn.Module):
         if self.run_type=='train':
             self.train()
         if self.run_type==0:
-            sys.exit("ERRORRRRRR")
+            sys.exit("ERROR")
         else:
             self.eval()
         
-        
-        #x=F.relu(self.fc1(x))
-        #x=F.relu(self.fc2(x))
-        #x=x.reshape(-1,42)
-        
-        ##x=x.reshape(-1,1,6,7)
-        #print(x.shape)
-        ##x=self.layer1(x)
-        
-        ##x=x.reshape(x.size(0),-1)
-        
-        ##x=self.layer2(x)
-        
+
         x=self.network_forward(x)
-        
-        #x=x.reshape(-1,42)
-        '''
-        x=F.relu(self.fc1(x))
-        x=self.bn_1(x.view(-1,128))
-        x=F.relu(self.fc2(x))
-        x=self.bn_2(x)
-        
-        x=F.sigmoid(self.fc3(x))
-        '''
+
         return x
     
     def epsilon_greedy(self,x):
@@ -133,10 +71,7 @@ class Agent(nn.Module):
                 self.train()
             else:
                 self.eval()
-            
-            
-            
-            
+
             x=self.forward(x)
         
         return x
@@ -163,13 +98,12 @@ class Agent(nn.Module):
                 
                 loss = F.smooth_l1_loss(outputs,targets)
                 
-                
+                #pdb.set_trace()
                 loss.backward()
                 self.opt.step()
             #print(f'epoch {1+i}/{epochs}')
             print(f'loss: {float(loss)}')
         
-
 
 class Agent_Dataset(Dataset):
     'Characterizes a dataset for PyTorch'
@@ -192,6 +126,7 @@ class Agent_Dataset(Dataset):
         boards_unrolled_next=torch.tensor(boards_unrolled_next,dtype=torch.float32)
         
         Q_values=self.agent.forward(boards_unrolled) # batch_size x 7
+        
         target_Q_values=Q_values.clone()
         Q_value_next_max=self.agent_target.forward(boards_unrolled_next).max(dim=1)[0]
         
@@ -199,7 +134,7 @@ class Agent_Dataset(Dataset):
         
         
         target_Q_value=rewards+self.agent.gamma*Q_value_next_max
-        target_Q_values[np.arange(len(chosen_actions)),chosen_actions.astype(np.int)]= target_Q_value
+        target_Q_values[np.arange(len(chosen_actions)),chosen_actions.astype(np.int)] = target_Q_value
         target_Q_values=Variable(target_Q_values.data,requires_grad=False)
         
         self.X=boards_unrolled
@@ -213,7 +148,6 @@ class Agent_Dataset(Dataset):
         'Generates one sample of data'
         # Select sample
         return self.X[index],self.target_Q_values[index]
-
 
 
 class conv_agent(Agent):
@@ -292,19 +226,9 @@ class vanilla_agent(Agent):
 
 
 
-
-
-
 if __name__ == "__main__":
     
-    '''
-    agent1=Agent();agent2=Agent()
-    g=Game(agent1,agent2)
-    g.play_game(n_of_turns=300)
-    '''
-    
-    replay_mem=2000
-    
+    replay_mem=500
     
     epsilon_start=.9
     epsilon_end=.3
@@ -312,32 +236,14 @@ if __name__ == "__main__":
     epsilon_step=(epsilon_end-epsilon_start)/replay_mem
     
     
-    learning_rate_start=.01
-    learning_rate_end=.001
-    
-    learning_rate_step=(learning_rate_end-learning_rate_start)/replay_mem
-    
-    
-    
-    
-    
-    targ_update=3
-    '''
-    agent1=Agent(epsilon=epsilon_start,gamma=.95,learning_rate=learning_rate_start);
-    agent2=Agent(epsilon=epsilon_start,gamma=.95,learning_rate=learning_rate_start)
-    g=Game(agent1,agent2)
-    
-    agent1.epsilon=epsilon_start
-    agent2.epsilon=epsilon_start
-    
-    '''
-    
-    
-    
-    agent1=conv_agent(epsilon=epsilon_start,gamma=.96,learning_rate=learning_rate_start);
-    agent2=conv_agent(epsilon=epsilon_start,gamma=.96,learning_rate=learning_rate_start)
-    agent1_target=copy.deepcopy(agent1);agent1_target.run_type='eval'
-    agent2_target=copy.deepcopy(agent2);agent2_target.run_type='eval'
+    learning_rate=.001
+
+    targ_update=5
+
+    agent1=conv_agent(epsilon=epsilon_start,gamma=.99,learning_rate=learning_rate);
+    agent2=conv_agent(epsilon=epsilon_start,gamma=.99,learning_rate=learning_rate)
+    agent1_target=conv_agent(epsilon=0,gamma=.99,learning_rate=learning_rate);agent1_target.load_state_dict(agent1.state_dict());agent1_target.run_type='eval'
+    agent2_target=conv_agent(epsilon=0,gamma=.99,learning_rate=learning_rate);agent2_target.load_state_dict(agent2.state_dict());agent2_target.run_type='eval'
     
     
     g=Game(agent1,agent2)
@@ -345,134 +251,24 @@ if __name__ == "__main__":
     agent1.epsilon=epsilon_start
     agent2.epsilon=epsilon_start
     
-    
-    
-    
-    
-    
-    
-    
+
     
     for i in range(replay_mem):
         #print(agent1.gamma)
-        g.play_game(n_of_turns=2000)
-# =============================================================================
-#         agent1_data=Agent_Dataset(agent=agent1,agent_target=agent1_target)
-#         agent2_data=Agent_Dataset(agent=agent2,agent_target=agent2_target)
-#         agent1_data.format_Q_data()
-#         agent2_data.format_Q_data()
-# =============================================================================
-        
+        g.play_game(n_of_turns=1000)
+   
         if replay_mem % 1==0:
-            agent1.optimize(2,400,agent1_target)
+            agent1.optimize(2,200,agent1_target)
         else:
-            agent2.optimize(2,400,agent2_target)
+            agent2.optimize(2,200,agent2_target)
         
-        if replay_mem % targ_update==0:
-            agent1_target=copy.deepcopy(agent1);agent1_target.run_type='eval'
-            agent2_target=copy.deepcopy(agent2);agent2_target.run_type='eval'
-# =============================================================================
-#         print('agent1 lr:' + str(agent1.opt.param_groups[0]['lr']))
-#         print('agent2 lr:' + str(agent2.opt.param_groups[0]['lr']))
-#         
-#         print('agent1 epsilon:' + str(agent1.epsilon))
-#         print('agent2 epsilon:' + str(agent2.epsilon))
-# =============================================================================
+        if replay_mem % targ_update*2==0:
+            agent1_target=conv_agent(epsilon=0,gamma=.99,learning_rate=learning_rate_start);agent1_target.load_state_dict(agent1.state_dict());agent1_target.run_type='eval'
+            agent2_target=conv_agent(epsilon=0,gamma=.99,learning_rate=learning_rate_start);agent2_target.load_state_dict(agent2.state_dict());agent2_target.run_type='eval'
+    
         
         print(f'replay_mem {1+i}/{replay_mem}............epsilon: {agent1.epsilon}......learning_rate: {agent1.opt.param_groups[0]["lr"]}....gamma:{agent1.gamma}')
-        
-        agent1.opt.param_groups[0]['lr']+=learning_rate_step
-        agent2.opt.param_groups[0]['lr']+=learning_rate_step
-        
+
         agent1.epsilon+=epsilon_step
         agent2.epsilon+=epsilon_step
 
-    
-
-
-#SmoothL1Loss
-
-#how to train when chosen action is random, and not on-policy
-#how to deal with 
-        
-        
-        
-        
-        
-        
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-'''
-
-    def create_mini_batch(self,memory,mb_size=32):
-        
-    
-        
-        mb_indices=np.random.choice((self.replay_mem_size+1),mb_size,replace=False)
-        
-        mini_batch=memory[mb_indices,:]
-        
-        
-        return mini_batch
-        
-    
-        def train_mini_batch(self,memory,mb_size,gamma):
-        
-        #criterion=nn.SmoothL1Loss()
-        
-        
-        
-        mini_batch=self.create_mini_batch(memory,mb_size)
-        mb_size=len(mini_batch)
-        
-            
-        #loss=criterion(Q_values,target_Q_values)
-        loss = F.smooth_l1_loss(Q_values,target_Q_values)
-        loss.backward()
-        opt.step()
-        #print(loss)
-'''
-
-
-
-
-
-# =============================================================================
-# if network_type=='convolution':
-#             
-#             self.layer1=nn.Sequential(nn.Conv2d(1, 16, kernel_size=(4,3), stride=1, padding=0),
-#                 nn.ReLU(),
-#                 nn.BatchNorm2d(16),
-#                 )
-#                 #nn.Linear(16*8,7),
-#                 #nn.Softmax())
-#             
-#             self.layer2=nn.Sequential(nn.Conv2d(16, 16, kernel_size=2, stride=1, padding=0),
-#                 nn.ReLU(),
-#                 nn.BatchNorm2d(16)
-#                 )
-#             
-#             
-#             self.layer3=nn.Sequential(nn.Linear(16*8,128),nn.ReLU(),nn.BatchNorm1d(128),nn.Linear(128,7),nn.Softmax())
-#             
-#             def reshape(self,arr):
-#                 return arr.reshape(-1,1,6,7)
-#         
-#         if network_type=='vanilla':
-#         
-#             self.layer1=nn.Sequential(nn.Linear(42,128),nn.ReLU(),nn.BatchNorm1d(128))
-#             self.layer2=nn.Sequential(nn.Linear(128,128),nn.ReLU(),nn.BatchNorm1d(128))
-#             self.layer3=nn.Sequential(nn.Linear(128,7),nn.Softmax())
-#         
-# 
-#             def reshape(self,arr):
-#                 return arr.reshape(arr.size(0),-1)
-# =============================================================================
